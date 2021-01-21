85546c3f54c5e3a5efe619270504544878896996
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2021-01-22 00:45:12 +0200

    provide PostgreSQL

---
+++ src/main/java/com/int20h2021/testtask/config/DevDataConfig.java
@@ -0,0 +1,33 @@
+package com.int20h2021.testtask.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@Profile("dev")
+public class DevDataConfig {
+    @Value("${spring.datasource.url}")
+    private String url;
+    @Value("${spring.datasource.driver-class-name}")
+    private String driverClassName;
+    @Value("${spring.datasource.username}")
+    private String username;
+    @Value("${spring.datasource.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        dataSource.setDriverClassName(driverClassName);
+        dataSource.setUrl(url);
+        dataSource.setPassword(password);
+        dataSource.setUsername(username);
+        return dataSource;
+    }
+}
