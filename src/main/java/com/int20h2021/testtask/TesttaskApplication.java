9bad871241d06f9a73ebb2fb088f8d8a739aa069
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2021-01-21 20:00:42 +0200

    allow CORS for all origins

--- src/main/java/com/int20h2021/testtask/TesttaskApplication.java
+++ src/main/java/com/int20h2021/testtask/TesttaskApplication.java
@@ -2,8 +2,10 @@ package com.int20h2021.testtask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
+@ServletComponentScan
 public class TesttaskApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/int20h2021/testtask/filter/AddCorsResponseHeaderFilter.java
@@ -0,0 +1,16 @@
+package com.int20h2021.testtask.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class AddCorsResponseHeaderFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        httpServletResponse.setHeader("access-control-allow-origin", "*");
+        chain.doFilter(request, response);
+    }
+}
