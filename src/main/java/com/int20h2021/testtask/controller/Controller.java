b278c8830198a35bfb1966e981cbc1283478497d
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2021-01-21 19:44:54 +0200

    delete redundant endpoints

--- src/main/java/com/int20h2021/testtask/controller/Controller.java
+++ src/main/java/com/int20h2021/testtask/controller/Controller.java
@@ -1,9 +1,7 @@
 package com.int20h2021.testtask.controller;
 
 import com.int20h2021.testtask.domain.json.common.Items;
-import com.int20h2021.testtask.domain.json.zakaz.ZakazResponse;
 import com.int20h2021.testtask.service.DataCollectionService;
-import com.int20h2021.testtask.domain.json.rozetka.RozetkaResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,21 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class Controller {
     private final DataCollectionService dataCollectionService;
-
-    @GetMapping("/rozetka")
-    public RozetkaResponse rozetka() {
-        return dataCollectionService.getRozetkaData();
-    }
-
-    @GetMapping("/metro")
-    public ZakazResponse metro() {
-        return dataCollectionService.getMetroData();
-    }
-
-    @GetMapping("/ecoMarket")
-    public ZakazResponse ecoMarket() {
-        return dataCollectionService.getEcoMarketData();
-    }
 
     @GetMapping("/")
     public Items all() {
