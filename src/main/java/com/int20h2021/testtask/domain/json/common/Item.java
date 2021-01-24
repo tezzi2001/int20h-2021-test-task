98266f7bd07ced61626247f0c3f7148ec0a82312
Bohdan Bondarenko <bohdan.bondarenko2001@gmail.com>
2021-01-24 20:26:35 +0200

    fix issue with weight

--- src/main/java/com/int20h2021/testtask/domain/json/common/Item.java
+++ src/main/java/com/int20h2021/testtask/domain/json/common/Item.java
@@ -8,6 +8,9 @@ import javax.persistence.Id;
 import javax.persistence.IdClass;
 import java.io.Serializable;
 
+import static java.lang.Math.ceil;
+import static org.apache.commons.math3.util.Precision.round;
+
 @Data
 @Entity
 @IdClass(ItemPK.class)
@@ -26,6 +29,7 @@ public class Item implements Serializable {
     private float pricePerKg;
 
     public Item(long id, String title, String href, String img, float price, String store, int weight, String producer) {
+        weight = parseWeight(weight);
         this.id = id;
         this.title = title;
         this.href = href;
@@ -38,10 +42,23 @@ public class Item implements Serializable {
     }
 
     public void setWeight(int weight) {
+        weight = parseWeight(weight);
         this.weight = weight == 0 ? 1000 : weight;
     }
 
     public void setProducer(String producer) {
         this.producer = producer == null ? "Вагова" : producer;
+    }
+
+    private int parseWeight(int weight) {
+        if (weight < 100) {
+            return (int) ceil(round(weight, -1));
+        } else if (weight < 1000) {
+            return (int) ceil(round(weight, -2));
+        } else if (weight < 10000) {
+            return (int) ceil(round(weight, -3));
+        } else {
+            return (int) ceil(round(weight, -4));
+        }
     }
 }
